651e96e1e090fe6f04ebb6bcc84119d2c2a959c4
Swayamjeet Padhi <swayamjeetpadhi7@gmail.com>
2023-10-13 00:31:41 +0530

    Update

--- Hill Cipher/Encryption.py
+++ Hill Cipher/Encryption.py
@@ -1,18 +1,23 @@
 import numpy as np
 
-key_matrix = [[2,2,2]]
-
 message=input("Enter a message: ")
+length=len(message)
+
 message_matrix=[]
 for char in message:
     m=[]
-    m.append(ord(char))
+    m.append(ord(char) - 31)
     message_matrix.append(m)
 
-cipher_matrix=np.multiply(key_matrix,message_matrix)
-t=""
-for num in range(len(message)):
-    a=str(chr(cipher_matrix[num][0]))
-    t=t+a
-    
-print(t)
+key = np.random.randint(5, size=(length,length))
+
+cipher_matrix=np.multiply(message_matrix, key)   
+
+cipher=""
+for n in range(length):
+    symbol=str(chr(cipher_matrix[n][0]))
+    cipher=cipher+symbol
+
+
+print("The cipher is: ")
+print(cipher)
