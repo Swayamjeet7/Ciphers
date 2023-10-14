f3dfb35982bc8617533669e46d5f5e9f3ecf88ce
Swayamjeet Padhi <swayamjeetpadhi7@gmail.com>
2023-10-14 23:56:15 +0530

    Update

--- Hill Cipher/Encryption.py
+++ Hill Cipher/Encryption.py
@@ -1,23 +1,33 @@
 import numpy as np
 
-message=input("Enter a message: ")
-length=len(message)
-
-message_matrix=[]
-for char in message:
-    m=[]
-    m.append(ord(char) - 31)
-    message_matrix.append(m)
-
-key = np.random.randint(5, size=(length,length))
-
-cipher_matrix=np.multiply(message_matrix, key)   
-
-cipher=""
-for n in range(length):
-    symbol=str(chr(cipher_matrix[n][0]))
-    cipher=cipher+symbol
+def key_generator(ln):
+    # key_matrix = np.random.randint(5, size=(ln,ln))
+    key_matrix=[[1,2],[2,3]]
+    key_text=""
+    for m in range(ln):
+        for n in range(ln):
+            key_text = key_text + chr(key_matrix[m][n] + 64)
+    return key_matrix,key_text
 
 
-print("The cipher is: ")
-print(cipher)
+def main():
+    message = input("Enter a message: ")
+    length = len(message)
+    message_matrix = []
+    for char in message:
+        message_matrix.append(ord(char) - 31)
+
+    key,key_text = key_generator(length)
+
+    cipher_matrix = np.matmul(message_matrix, key)   
+
+    cipher = ""
+    for n in range(length):
+        symbol = str(chr(cipher_matrix[n] ))
+        cipher = cipher+symbol
+
+    print(f"The cipher is: {cipher}")
+    print(f"The key used for encryption is: {key_text}")
+
+if __name__=="__main__":
+    main()
