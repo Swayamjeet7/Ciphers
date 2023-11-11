ac73017a65567afabc27e029d82ee458a705260b
Swayamjeet Padhi <swayamjeetpadhi7@gmail.com>
2023-11-11 15:38:04 +0530

    Caesar cipher fix

--- Caesar Cipher/Decryption.py
+++ Caesar Cipher/Decryption.py
@@ -1,13 +1,17 @@
-# This program is for decryption of a password 
-password=input("Enter a password: ")
+# This program is for decryption of the encrypted message 
+
+password=input("Enter the encrypted message: ")
 key=int(input("Enter the key used for encryption: "))
 message=""
+
 for char in password:
-    if char==" ":
-        msg=" "
-    else:
-        value=ord(char)
-        new_value=value+key
-        msg=chr(new_value)
+    key = key % 126
+    value=ord(char)
+    new_value=value-key
+    
+    if new_value <= 31 :
+        new_value = 126 - (31 - new_value)
+    
+    msg=chr(new_value)
     message=message+msg
 print(message) 
--- Caesar Cipher/Encryption.py
+++ Caesar Cipher/Encryption.py
@@ -1,13 +1,18 @@
 # This program is for encryption of a message using a key
+# In this code the ASCII values considered are from 32('SPACE') to 126('~')
+
 message=input("Enter a message: ")
 key=int(input("Enter a key value: "))
 password=""
+
 for char in message:
-    if char==" ":
-        ps=" "
-    else:
-        value=ord(char)
-        new_value=value-key
-        ps=chr(new_value)
-    password=password+ps
+    value =  ord(char)
+    key = key % 126
+    new_value = value + key 
+    
+    if new_value > 126 :
+        new_value = (new_value % 126) + 31
+    
+    ps = chr(new_value)
+    password = password+ps
 print(password)  
